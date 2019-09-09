b58f352948f43ccb3d4fa2afe4cf37d5c69a726e
unknown <jrnival.gss@gmail.com>
2019-09-09 07:53:20 -0700

    Initial push from local

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function() {
+    return gulp.src('./scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./scss/**/*.scss', gulp.series('sass'));
+});
+
+exports.default = gulp.series('watch');
